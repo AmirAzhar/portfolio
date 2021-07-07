d6a244e2d7e79f40f1e9bf236f635d12711cb41a
Amir Azhar <amir97azhar@gmail.com>
2021-07-08 00:49:37 +0800

    Testing react spring

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,18 @@
 import "./home.css";
 
+import { useSpring, animated } from "react-spring";
+import { useState } from "react";
+
 const Home = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
+  const styles = useSpring({
+    loop: true,
+    to: [
+      { opacity: 1, color: "#ffaaee" },
+      { opacity: 0, color: "rgb(14,26,19)" },
+    ],
+    from: { opacity: 0, color: "red" },
+  });
+  return <animated.div style={styles}>I will fade in and out</animated.div>;
 };
 
 export default Home;
