a8367b64d8779ccd0a0c1b4a37faa3dd60f243a8
AmirAzhar <itooth55@gmail.com>
2023-07-08 21:20:35 +0800

    [FIX] Refactor project modal

--- src/screens/portfolio/Portfolio.js
+++ src/screens/portfolio/Portfolio.js
@@ -1,55 +1,25 @@
+// Styles
 import "./portfolio.css";
 
-// Import utilities
-import { useState } from "react";
-import { motion } from "framer-motion";
-
-//Import components
+// Components
 import ProjectModal from "../../components/projectModal";
 import ExperienceTimeline from "../../components/experienceTimeline";
 import ProjectAccordion from "../../components/projectAccordian";
+
+// Resources
 import { projects } from "../../resources/data";
 
 const Portfolio = () => {
-  const [modalShow, setModalShow] = useState(false);
-  const [state, setState] = useState({
-    title: "",
-    thumbnail: "",
-    alt: "",
-    body: "",
-    subtitle: "",
-    contentImages: [],
-  });
-
   return (
     <div className="content">
-      <div className="header"> Past Projects </div>
-
+      <div className="header">Past Projects</div>
       <div className="projectGrid">
         {projects.map((proj) => (
-          <motion.img
-            className="projectThumbnail"
-            src={proj.thumbnail}
-            alt={proj.alt}
-            responsive
-            rounded
-            onClick={() => {
-              setModalShow(true);
-              setState(proj);
-            }}
-            whileHover={{ scale: 1.1, opacity: 1 }}
-          />
+          <ProjectModal content={proj} />
         ))}
-        <ProjectModal
-          content={state}
-          show={modalShow}
-          onHide={() => setModalShow(false)}
-        />
       </div>
-
       <ProjectAccordion />
       <div className="header"> Experience & Education</div>
-
       <ExperienceTimeline />
     </div>
   );
