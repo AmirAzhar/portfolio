1715b3e6693a511219230a35040de25049b3d13e
AmirAzhar <itooth55@gmail.com>
2023-07-11 19:18:07 +0800

    [ADD] Back button

--- src/screens/spa/Spa.js
+++ src/screens/spa/Spa.js
@@ -7,6 +7,9 @@ import Portfolio from "../portfolio";
 import Blog from "../blog";
 import Contact from "../contact";
 
+// Icons
+import { RiArrowGoBackFill } from "react-icons/ri";
+
 function Spa() {
   return (
     <div className="spa">
@@ -14,6 +17,9 @@ function Spa() {
       <Portfolio />
       <Blog />
       <Contact />
+      <a className="back" href="/">
+        <RiArrowGoBackFill size={"1.5rem"} />
+      </a>
     </div>
   );
 }
